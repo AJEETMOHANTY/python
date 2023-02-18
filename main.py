a83a9bd2855cd5987fcfc53724dbb4eba7a756ee
AJEET <ajeetmohanty2004@gmail.com>
2023-02-19 01:20:29 +0530

    function

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def my_fun(x,y):
+    return x+y
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
