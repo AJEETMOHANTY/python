7b5d3cb49a598a5cdc3bad0108b44af641b55f01
AJEET <ajeetmohanty2004@gmail.com>
2023-02-19 23:26:31 +0530

    If Else Statement

---
+++ if_else.py
@@ -0,0 +1,8 @@
+# To check the entered email and password is correct or not.
+
+email = input("enter the email address")
+password = input("password")
+if email=="ajeetmohanty2004@gmail.com" and password=="12345":
+    print("welcome")
+else:
+    print("incorrect credential")
